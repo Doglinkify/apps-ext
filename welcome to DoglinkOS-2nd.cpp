36d8d3441dbbf162363517974141411f42091abf
shc2012 <shc20122024@outlook.com>
2025-05-30 17:41:17 +0800

    cpp

---
+++ welcome to DoglinkOS-2nd.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+   cout <<Welcome to DoglinkOS-2nd<< endl;//三种语言
+   cout <<欢迎来到DoglinkOS-2nd<< endl;
+   cout <<歡迎來到DoglinkOS-2nd<< endl;
+   return 0;
+}
