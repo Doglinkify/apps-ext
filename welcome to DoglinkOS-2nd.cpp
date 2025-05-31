7fb4b12a1066d26bb477732ca466c124dc6968a1
shc2012 <shc20122024@outlook.com>
2025-05-31 14:00:32 +0800

    new

--- welcome to DoglinkOS-2nd.cpp
+++ welcome to DoglinkOS-2nd.cpp
@@ -12,9 +12,10 @@ int main()
     while(true) {
         cout << "\n请选择操作：" << endl;
         cout << "1. Knowabout DoglinkOS-2nd 了解DoglinkOS-2nd 瞭解DoglinkOS-2nd" << endl;
-        cout << "2.  Developer's Guide 开发者指南 開發者指南" << endl;
-        cout << "3. exit 退出 退出" << endl;
-        cout << "choice （1-3）请输入选项  (1-3) 請輸入選項 （1-3）: ";
+        cout << "2. Developer's Guide 开发者指南 開發者指南" << endl;
+        cout << "3. Example of app 程序示例 程序示例" << endl;
+        cout << "4. Exit 退出 退出 "<< endl;
+        cout << "choice （1-4）请输入选项 (1-4) 請輸入選項 （1-4）: ";
         
         cin >> choice;
         
@@ -31,8 +32,15 @@ int main()
                 cout <<""<< endl;
                 cout <<""<< endl;
                 break;
-                
+
             case 3:
+                cout <<""<< endl;
+                cout <<""<< endl;
+                cout <<""<< endl;
+                cout <<""<< endl;
+                break;
+                
+            case 4:
                 return 0;
                 
             default:
